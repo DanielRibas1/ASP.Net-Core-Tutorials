a6676330fe3ef9ca4f316247e1246a071b972c90
DanielRibas1 <daniribas01@gmail.com>
2018-05-07 19:58:13 +0200

    Update the Create page

---
+++ EFCore/ContosoUniversity/Pages/Students/CreateVM.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class CreateVMModel : PageModel
+    {
+        [BindProperty]
+        public StudentVM StudentVM { get; set; }
+
+        private readonly SchoolContext _context;
+
+        public CreateVMModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var entry = _context.Add(new Student());
+            entry.CurrentValues.SetValues(StudentVM);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+    }
+}
