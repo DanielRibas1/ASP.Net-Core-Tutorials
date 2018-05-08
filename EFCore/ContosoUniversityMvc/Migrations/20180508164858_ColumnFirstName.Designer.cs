4b7e28ce5a28a08439d2f57be768240d58ec45c8
DanielRibas1 <daniribas01@gmail.com>
2018-05-08 18:49:20 +0200

    Customize the Data Model by Using Attributes

--- EFCore/ContosoUniversityMvc/Migrations/20180508164858_ColumnFirstName.Designer.cs
+++ EFCore/ContosoUniversityMvc/Migrations/20180508164858_ColumnFirstName.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversityMvc.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180508164858_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
