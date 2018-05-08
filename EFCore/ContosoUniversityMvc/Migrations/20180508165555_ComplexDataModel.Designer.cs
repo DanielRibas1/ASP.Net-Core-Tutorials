3e756839b9ea160652bab8688829d4c0c5a2332c
DanielRibas1 <daniribas01@gmail.com>
2018-05-08 18:56:54 +0200

    Add a migration

--- EFCore/ContosoUniversityMvc/Migrations/20180508165555_ComplexDataModel.Designer.cs
+++ EFCore/ContosoUniversityMvc/Migrations/20180508165555_ComplexDataModel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosoUniversityMvc.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180508165555_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
