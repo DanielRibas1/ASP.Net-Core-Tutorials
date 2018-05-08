dd51f9294333a48d955ef4178c2df71bada9ffc3
DanielRibas1 <daniribas01@gmail.com>
2018-05-08 19:35:18 +0200

    Create and customize migration code

--- EFCore/ContosoUniversityMvc/Migrations/20180508173420_Inheritance.Designer.cs
+++ EFCore/ContosoUniversityMvc/Migrations/20180508173420_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace ContosoUniversityMvc.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180508173420_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
